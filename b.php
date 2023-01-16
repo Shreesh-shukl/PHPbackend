f6f6292602fc1627809649be026583835c163070
Shreesh Shukla <shreesh8210@gmail.com>
2023-01-15 16:27:24 -0800

    first commit

---
+++ b.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang='en'>
+<head>
+<meta charset='UTF-8'>
+<meta http-equiv='X-UA-Compatible' content='IE=edge'>
+<meta name='viewport' content='width=device-width, initial-scale=1.0'>
+<title>First Css</title>
+</head>
+<style>
+*{
+    margin: 0;
+    padding: 0;
+    box-sizing: border-box;
+}
+    .container{
+        max-width: 80%;
+        background-color: red;
+        margin: auto;
+        padding: 23px;
+    }
+</style>
+<body>
+<div class="container">
+    <h1>Lets learn Php</h1>Hllo I am good</div>
+</body>
+</html>
